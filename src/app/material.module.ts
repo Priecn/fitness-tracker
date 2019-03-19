b88dbc80ce21c289c21e86ec977dbceca658f7cd
unknown <prince.silverarrow@gmail.com>
2019-03-20 01:46:37 +0530

    simple signup form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,31 @@
 import { NgModule } from '@angular/core';
+import {
+  MatFormFieldModule,
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatCheckboxModule
+} from '@angular/material';
 @NgModule({
-    imports: [],
-    exports: []
+  imports: [
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule
+  ]
 })
 export class MaterialModule {}
